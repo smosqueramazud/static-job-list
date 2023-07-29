a125c17e19901854094719bc7c5aa87462642858
SMosqueraCoink <s.mosquera@coink.com>
2023-07-29 18:50:55 -0500

    se crea el routing @Author Sebastian Mosquera Mazud

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+    import('./pages/home/home.component').then(
+      c => c.HomeComponent
+    )
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
