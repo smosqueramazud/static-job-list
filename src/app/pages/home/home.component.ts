d68805f1de5214986aa00f9955fa1e4db50f06d7
SMosqueraCoink <s.mosquera@coink.com>
2023-07-29 19:08:21 -0500

    se obtiene la data del archivo js @Author Sebastian Mosquera Mazud

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DataServicesService } from 'src/app/services/data-services.service';
+import { catchError, throwError } from 'rxjs';
+import { Person } from 'src/app/models/person';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +11,26 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  arrayPersons: Person[] | undefined;
+
+  ngOnInit() {
+    this.getInfoPersons();
+  }
+
+  dataService: DataServicesService = inject(DataServicesService);
+
+  async getInfoPersons(){
+    await this.dataService.getInfoPersons()
+    .pipe(
+      catchError((err: any) => {
+        return throwError(err);
+      }),
+    )
+    .subscribe(res => {
+      this.arrayPersons = res;
+      console.log(this.arrayPersons)
+    });
+  }
 }
